2475d283c40b26887b7089b323cac096ff5a2635
SuryaHanish <22a91a4422@aec.edu.in>
2024-11-16 06:09:50 +0530

    cloudinary

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -2,13 +2,16 @@ import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 
 // Load environment variables from .env file
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 
 // Debugging: Check if the environment variables are being loaded correctly
-//console.log("Cloudinary Config:");
-//console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
-//console.log("API Key:", process.env.CLOUDINARY_API_KEY);
-//console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
+console.log("Cloudinary Config:");
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
 console.log("Cloudinary connected");
 
 // Cloudinary configuration
